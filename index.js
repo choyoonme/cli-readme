0248b6bbc2d9ffa4233d3a0ffd81b0003c5d11f3
Jen Cho <99620655+choyoonme@users.noreply.github.com>
2022-05-02 15:39:18 -0400

    add type name message

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
 const inquirer = require('inquirer');
-
-//prompt user for name
-//ask what language they speak
-//print "greeting name"
+const ListPrompt = require('inquirer/lib/prompts/list');
 
 const questions = [{
-        type: 'input',
-        name: 'name',
-        message: 'what is your first name?',
-        validate(value) {
-            const pass = value.length > 0;
-            if (pass) {
-                return true;
-            }
-            return 'cannot leave blank';
-        },
-
+        //initial questions
+        type: "input",
+        name: "title",
+        message: "Enter name of your repository:"
+    }, {
+        type: "input",
+        name: "gitHubUserName",
+        message: "Enter GitHub username:"
+    }, {
+        type: "input",
+        name: "gitHubLink",
+        message: "Provide link to GitHub profile:"
+    }, {
+        type: "input",
+        name: "emailAddress",
+        message: "Enter your email address:"
+    },
+    //Readme section questions
+    {
+        type: "input",
+        name: "description",
+        message: "Enter brief description of project:"
     },
     {
-        type: 'list',
-        name: 'language',
-        message: 'what language do you speak?',
-        choices: ['english', 'spanish', 'korean', 'swedish'],
-
+        type: "input",
+        name: "tableOfContents",
+        message: "List a Table of Contents, if any:"
     },
-
+    {
+        type: "input",
+        name: "installation",
+        message: "Briefly describe installation process:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Briefly describe usage of project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "License:",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Name of contributor(s):"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Test?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Any questions?"
+    },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-    if (answers.language === 'korean') {
-        console.log('ahnyong', answers.name);
-    }
-    if (answers.language === 'swedish') {
-        console.log('hej', answers.name)
-    }
-    if (answers.name === 'phil') {
-        console.log('hey boss!');
-    }
 
-})
+inquirer.prompt(questions).then((answers) => {
+    console.log(answers.message);
+});
